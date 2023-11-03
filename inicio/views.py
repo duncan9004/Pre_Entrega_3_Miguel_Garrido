580b79c3bb54713a0e1d62ffa0a9d587ec772261
duncan9004 <m_garrido_4@hotmail.com>
2023-11-03 12:03:17 -0300

    msj

--- inicio/views.py
+++ inicio/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render,redirect
 from inicio.models import Paciente
 from .forms import PacienteForm, CitaForm, TratamientoForm,BusquedaForm
+
 def inicio(request):
     
   
     return render(request,"inicio/inicio.html",{})
-
-
 
 def buscar(request):
     resultados = None
@@ -15,7 +14,7 @@ def buscar(request):
         if form.is_valid():
             termino = form.cleaned_data.get('termino')
             resultados = Paciente.objects.filter(nombre__icontains=termino)  
-            
+             
     return render(request, 'buscar_resultados.html', {'resultados': resultados, 'form': form})
 
 def agregar_paciente(request):
